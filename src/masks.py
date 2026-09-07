1062e1ea973fb58005036521fa1082381b222138
Exi <email@example.com>
2026-09-07 12:06:57 +0300

    Добавлено логирование модулей masks и utils

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,41 @@
+import logging
+from pathlib import Path
+
+logs_dir = Path(__file__).resolve().parent.parent / "logs"
+logs_dir.mkdir(exist_ok=True)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler(
+    logs_dir / "masks.log",
+    mode="w",
+    encoding="utf-8",
+)
+
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_number: str) -> str:
-    """Функция маскирует номер карты"""
+    """Функция маскирует номер карты."""
     if not card_number:
+        logger.error("Передан пустой номер карты")
         return ""
+
+    logger.info("Номер карты успешно замаскирован")
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(account: str) -> str:
-    """Функция скрывает номер счета"""
+    """Функция скрывает номер счета."""
+    if not account:
+        logger.error("Передан пустой номер счета")
+        return ""
+
+    logger.info("Номер счета успешно замаскирован")
     return f"**{account[-4:]}"
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,26 @@
 import json
+import logging
+from pathlib import Path
 from typing import Any
+
+logs_dir = Path(__file__).resolve().parent.parent / "logs"
+logs_dir.mkdir(exist_ok=True)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler(
+    logs_dir / "utils.log",
+    mode="w",
+    encoding="utf-8",
+)
+
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def load_transactions(file_path: str) -> list[dict[str, Any]]:
@@ -9,9 +30,16 @@ def load_transactions(file_path: str) -> list[dict[str, Any]]:
             data = json.load(file)
 
         if isinstance(data, list):
+            logger.info("Транзакции успешно загружены из файла %s", file_path)
             return data
 
+        logger.error("JSON-файл %s содержит данные не в виде списка", file_path)
         return []
 
-    except (FileNotFoundError, json.JSONDecodeError):
+    except FileNotFoundError:
+        logger.error("Файл %s не найден", file_path)
+        return []
+
+    except json.JSONDecodeError:
+        logger.error("Ошибка чтения JSON-файла %s", file_path)
         return []
